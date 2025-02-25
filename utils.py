757fcecc4c9868fd0ec5204252772bf796472717
hungyi <illusion.dark@gmail.com>
2025-02-25 16:44:09 +0800

    Fix get_norm_stats()

--- utils.py
+++ utils.py
@@ -145,17 +145,23 @@ def get_norm_stats(dataset_dir, num_episodes):
             action = root['/action'][()]
         all_qpos_data.append(torch.from_numpy(qpos))
         all_action_data.append(torch.from_numpy(action))
-    all_qpos_data = torch.stack(all_qpos_data)  # shape: (num_episodes, num_steps, state_dim)
-    all_action_data = torch.stack(all_action_data)  # shape: (num_episodes, num_steps, state_dim)
+    #all_qpos_data = torch.stack(all_qpos_data)  # shape: (num_episodes, num_steps, state_dim)
+    #all_action_data = torch.stack(all_action_data)  # shape: (num_episodes, num_steps, state_dim)
+    all_qpos_data = torch.cat(all_qpos_data, dim=0)  # shape: (num_episodes*num_steps, state_dim)
+    all_action_data = torch.cat(all_action_data, dim=0)  # shape: (num_episodes*num_steps, state_dim)
 
     # normalize action data
-    action_mean = all_action_data.mean(dim=[0, 1], keepdim=True)  # shape: (1, 1, state_dim)
-    action_std = all_action_data.std(dim=[0, 1], keepdim=True)  # shape: (1, 1, state_dim)
+    # action_mean = all_action_data.mean(dim=[0, 1], keepdim=True)  # shape: (1, 1, state_dim)
+    # action_std = all_action_data.std(dim=[0, 1], keepdim=True)  # shape: (1, 1, state_dim)
+    action_mean = all_action_data.mean(dim=0, keepdim=True)  # shape: (1, state_dim)
+    action_std = all_action_data.std(dim=0, keepdim=True)  # shape: (1, state_dim)
     action_std = torch.clip(action_std, 1e-2, np.inf) # clipping
 
     # normalize qpos data
-    qpos_mean = all_qpos_data.mean(dim=[0, 1], keepdim=True)
-    qpos_std = all_qpos_data.std(dim=[0, 1], keepdim=True)
+    # qpos_mean = all_qpos_data.mean(dim=[0, 1], keepdim=True)
+    # qpos_std = all_qpos_data.std(dim=[0, 1], keepdim=True)
+    qpos_mean = all_qpos_data.mean(dim=0, keepdim=True)
+    qpos_std = all_qpos_data.std(dim=0, keepdim=True)
     qpos_std = torch.clip(qpos_std, 1e-2, np.inf) # clipping
 
     stats = {"action_mean": action_mean.numpy().squeeze(), "action_std": action_std.numpy().squeeze(),
